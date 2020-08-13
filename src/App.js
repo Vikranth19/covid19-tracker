97a27f5409609759d75618aa8f79bc94af261673
Vikranth19 <64738319+Vikranth19@users.noreply.github.com>
2020-08-12 20:41:09 -0700

    added all the files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import Cards from './components/Cards/Cards';
 
-import {Cards, Chart, CountryPicker} from './components'
+import {Cards, Chart, CountryPicker, Footer} from './components'
 import styles from './App.module.css'
 import { fetchData } from './api'
 
@@ -49,6 +49,7 @@ class App extends React.Component {
          <Cards data={data}/>
          <CountryPicker handleCountryChange={this.handleCountryChange}/>
          <Chart data={data} country={country}/>
+         <Footer/>
       </div>
     );
   }
